932a7748435d3305ae513d47bb3798671317adb5
mocharsyil <moch.arsyil@gmail.com>
2022-04-20 09:46:07 +0700

    edit user entity

--- src/main/java/id/ac/uinsgd/forgotpassword/entity/User.java
+++ src/main/java/id/ac/uinsgd/forgotpassword/entity/User.java
@@ -15,7 +15,7 @@ import java.util.Set;
 
 @Data
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "t_user"))
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"), name= "t_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
